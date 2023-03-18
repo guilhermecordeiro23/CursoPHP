eaff29d91b488b69685815f1ccc7b01eb73b68c2
Guilherme Cordeiro Cavalcanti <guivifla2005@gmail.com>
2023-03-17 22:11:13 -0300

    atividade

---
+++ ex003/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tipos primitivos em PHP</title>
+</head>
+<body>
+    <h1>Testes de tipos primitivos</h1>
+    <hr>
+    <?php 
+        //0x = hexadecimal, 0b = binário, 0 = octal
+        //$num = 0x1A;
+        //echo "O valor da variável é $num "; 
+
+        //$v = "Guilherme";
+        //var_dump($v);
+
+        //$num =(int)3e2; //3 x 10(2) coerção
+        //var_dump($num);
+
+        
+
+    ?>
+</body>
+</html>
